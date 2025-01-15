ecbe7ea06717237ba39f502bf4553abcec0ebc93
Elena Masserini <e.masserini2@campus.unimib.it>
2025-01-15 12:25:59 +0100

    Update zip download for specified commit

--- check_not_indexed_repositories.py
+++ check_not_indexed_repositories.py
@@ -2,38 +2,20 @@ import requests
 import csv
 import zipfile
 import re
-
-
-NOT_INDEXED_FILE_NAME = "not_indexed.csv"
-CSV_SEPARATOR = ';'
-CHATBOTS_FILE = 'chatbots.csv'
-NOT_CHATBOTS_FILE = 'not_chatbots.csv'
-EMPTY_REPOSITORIES_FILE = 'empy_repositories'
+from utils import download_zip, clean_zip
 import os
 
-# Download GitHub repository
-def download_zip(repo_name, branch):
-    download_url = f"https://github.com/{repo_name}/archive/refs/heads/{branch}.zip"
-    print(download_url)
-    
-    try: 
-        response = requests.get(download_url, timeout=300)
-        if response.status_code != 200:
-            print(f'Error in repository {repo_name} ZIP download: {response.status_code}')
-            return -1
-        else:
-            zip_path = f"chatbot_zip/{repo_name.replace('/', '_')}.zip"
-            with open(zip_path, "wb") as f:
-                f.write(response.content)
-            return zip_path
-    except requests.exceptions.Timeout:
-        print(f"Timeout in repository {repo_name} ZIP download")
-        return 0
+
+NOT_INDEXED_FILE_NAME = "not_indexed-2025.csv"
+CSV_SEPARATOR = ';'
+CHATBOTS_FILE = 'chatbots-2025.csv'
+NOT_CHATBOTS_FILE = 'not_chatbots-2025.csv'
+EMPTY_REPOSITORIES_FILE = 'empty_repositories-2025-csv'
+ZIP_DIRECTORY = 'chatbot_zip-2025'
+
     
 
-# Clean zip from cache and models (except most recent)
-
-
+# Check if repository is chatbot
 def find_keyword_in_repo(keyword, repo_zip_path):
     domain_files = []
     repo =  zipfile.ZipFile(repo_zip_path, 'r')
@@ -50,15 +32,12 @@ def find_keyword_in_repo(keyword, repo_zip_path):
     return domain_files
 
 
-
-
-
 def main():
 
+    # Open files
     not_indexed_file = open(NOT_INDEXED_FILE_NAME, 'r')
     reader = csv.DictReader(not_indexed_file, delimiter=CSV_SEPARATOR)
     repositories = list(reader)
-
 
     ncb_file = open(NOT_CHATBOTS_FILE, 'a', newline='')
     ncb_csv = csv.DictWriter(ncb_file, fieldnames= reader.fieldnames, delimiter=CSV_SEPARATOR)
@@ -67,31 +46,39 @@ def main():
     cb_headers =  reader.fieldnames + ['domain-files']
     cb_csv = csv.DictWriter(cb_file, fieldnames=cb_headers, delimiter=CSV_SEPARATOR)
     
-    
+    if not os.path.isdir(ZIP_DIRECTORY):
+        os.makedirs(ZIP_DIRECTORY)
 
-    for repo in repositories[300:400]:
+    for repo in repositories: 
         try:
-            zip_path = download_zip(repo['full-name'], repo['default-branch'])
+            # Download zip
+            zip_path = download_zip(ZIP_DIRECTORY, repo['full-name'], repo['last-commit'])
             print('Download completed')
             if zip_path != -1:
+                # Chatbot check
                 domain_files = find_keyword_in_repo('intents', zip_path)
-                print(domain_files)
+                # Not chatbot
                 if not domain_files:
                     print(f"{repo['full-name']}: not chatbot")
                     ncb_csv.writerow(repo)
-                    ncb_file.flush()
-                    os.remove(zip_path)
+                    #Remove zip
+                    os.remove(zip_path) 
                 else:
+                    # Chatbot
                     print(f"{repo['full-name']}: chatbot")
                     repo['domain-files'] = domain_files
                     cb_csv.writerow(repo)
-                    cb_file.flush()
+                    # Clean zip
+                    clean_zip(zip_path)
+
+        # Exception: empty repository
         except zipfile.BadZipFile as e:
             print(f"Empty repository: {e}")
             empty_repo_file = open(EMPTY_REPOSITORIES_FILE, 'a', newline='')
             empty_csv = csv.DictWriter(empty_repo_file, fieldnames=reader.fieldnames, delimiter=CSV_SEPARATOR)
             empty_csv.writerow(repo)
             empty_repo_file.close()
+            os.remove(zip_path)
 
     cb_file.close()
     ncb_file.close()
